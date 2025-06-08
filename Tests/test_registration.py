650b27ac89e16fb45d5e0206b7915cf96e6ba38e
Lekerest <ikhritankov@yandex.ru>
2025-06-08 22:16:06 +0700

    Update test_registration.py

--- Tests/test_registration.py
+++ Tests/test_registration.py
@@ -1,1 +1,16 @@
-#333
+import random
+from selenium.webdriver.common.by import By
+from selenium import webdriver
+from selenium.webdriver.support import expected_conditions
+from selenium.webdriver.support.wait import WebDriverWait
+import time
+
+driver = webdriver.Chrome()
+driver.maximize_window() # полноэкранный режим
+driver.get("https://stellarburgers.nomoreparties.site/")
+
+login = 'Ivan_Hritankov_25_' + str(random.randint(99, 1000))
+password = random.randint(100000, 999999)
+
+
+driver.quit()
